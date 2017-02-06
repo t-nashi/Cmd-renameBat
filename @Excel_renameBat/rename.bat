18224cbbe0044838a3625b5e664ca3b9d320633f
TsutomuTakanashi <t_19nashi@yahoo.co.jp>
2017-02-07 04:03:58 +0900

    サンプルファイル一式追加

---
+++ @Excel_renameBat/rename.bat
@@ -0,0 +1,5 @@
+ren "C:\@Excel_renameBat\samplePNG1.png" 1.png
+ren "C:\@Excel_renameBat\samplePNG2.png" 2.png
+ren "C:\@Excel_renameBat\samplePNG3.png" 3.png
+ren "C:\@Excel_renameBat\samplePNG4.png" 4.png
+pause
